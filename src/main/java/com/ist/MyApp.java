cebebba51d7194eb3dd9430f8fd4ebaa339c2b01
saife <bsaifon323@gmail.com>
2023-03-25 02:33:35 +1100

    Add of comment

--- src/main/java/com/ist/MyApp.java
+++ src/main/java/com/ist/MyApp.java
@@ -1,8 +1,14 @@
 package com.ist;
 
+/*
+ * This is a comment
+ */
+
 public class MyApp {
 
     public static void main(String[] args) {
+
+        // Another comment
         System.out.println("Hello world");
     }
 }
